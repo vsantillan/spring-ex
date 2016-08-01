9b1ece888f28cbfc1797e479f96f6d88f56379f3
visantillan <visantillan@adeamexico.com.mx>
2016-08-01 18:51:34 -0500

    Files added

---
+++ src/main/java/mx/com/adea/springex/controllers/HelloController.java
@@ -0,0 +1,34 @@
+package mx.com.adea.springex.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Crated by Victor Santillán on 01/08/2016.
+ * visantillan@adeamexico.com.mx
+ */
+@Controller
+public class HelloController {
+
+  private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
+  @RequestMapping(value = "/hola", method = RequestMethod.GET)
+  public String hola(@RequestParam(value = "name", required = false, defaultValue = "World")
+                         String name, Model model) {
+    model.addAttribute("recipient", name);
+    return "hola";
+  }
+
+  @RequestMapping(value = "/index", method = RequestMethod.GET)
+  public String fecha(@RequestParam(value = "name", required = false, defaultValue = "World")
+                          String name) {
+    logger.debug("WORKS");
+    return "index";
+  }
+}
