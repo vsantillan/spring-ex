37ee2578f171e5e064a51ce43cecd8713bf1c701
Victor Santillán <viktor.santillan@gmail.com>
2016-08-14 23:09:14 -0500

    Conexión a Postgres

--- src/main/java/mx/com/adea/springex/controllers/HelloController.java
+++ src/main/java/mx/com/adea/springex/controllers/HelloController.java
@@ -1,7 +1,5 @@
 package mx.com.adea.springex.controllers;
 
-import mx.com.adea.springex.beans.UsuarioWebmxEntity;
-import mx.com.adea.springex.dao.UsuarioDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 /**
  * Crated by Victor Santillán on 01/08/2016.
  * visantillan@adeamexico.com.mx
@@ -20,21 +16,13 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-  private final UsuarioDAO userDao;
-
   private final Logger logger = LoggerFactory.getLogger(HelloController.class);
-
-  @Autowired
-  public HelloController(UsuarioDAO userDao) {
-    this.userDao = userDao;
-  }
 
   @RequestMapping(value = "/hola", method = RequestMethod.GET)
   public String hola(@RequestParam(value = "name", required = false, defaultValue = "World")
                          String name, Model model) {
 
     model.addAttribute("recipient", name);
-    List<UsuarioWebmxEntity> list = userDao.list();
     return "hola";
   }
 
--- src/main/java/mx/com/adea/springex/services/UsuarioManager.java
+++
@@ -1,8 +0,0 @@
-package mx.com.adea.springex.services;
-
-/**
- * Crated by Victor Santillán on 06/08/2016.
- * visantillan@adeamexico.com.mx
- */
-public class UsuarioManager {
-}
