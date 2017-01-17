8bbe4d45ce13836d80f55246dc79839a3f278c7b
visantillan <visantillan@adeamexico.com.mx>
2017-01-17 17:04:05 -0600

    funcionando con extjs y spring

--- src/main/java/mx/com/adea/projects/controllers/HelloController.java
+++ src/main/java/mx/com/adea/projects/controllers/HelloController.java
@@ -1,43 +1,29 @@
 package mx.com.adea.projects.controllers;
 
-import mx.com.adea.projects.dao.EmployeeDao;
+import mx.com.adea.projects.beans.Greeting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Crated by Victor Santillán on 01/08/2016.
  * visantillan@adeamexico.com.mx
  */
-@Controller
+@RestController
 public class HelloController {
-
-	private final EmployeeDao employeeDao;
 
 	private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-	@Autowired
-	public HelloController(EmployeeDao employeeDao) {
-		this.employeeDao = employeeDao;
-	}
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping(value = "/hola", method = RequestMethod.GET)
-	public String hola(@RequestParam(value = "name", required = false, defaultValue = "World")
-												 String name, Model model) {
-		logger.debug(name);
-
-		model.addAttribute("recipient", name);
-		return "hola";
-	}
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String fecha() {
-		logger.debug("WORKS");
-		return "index";
+	@RequestMapping("/greeting.action")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
 	}
 }
